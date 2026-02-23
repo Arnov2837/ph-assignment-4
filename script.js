0f5066cfe7330b8a47155f45b123fc9542121377
Arnov2837 <Shohanurroshid2@gmail.com>
2026-02-23 19:07:10 +0300

    added html

---
+++ script.js
@@ -0,0 +1,8 @@
+let totalJob = document.getElementById("total");
+let jobInterview = document.getElementById("job-interview");
+let jobRejected = document.getElementById("job-rejected");
+
+
+
+let interview = []
+let rejected = []
